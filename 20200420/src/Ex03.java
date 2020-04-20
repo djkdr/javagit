95a2d2d43fe4f318e87b9914395a04dd5a3b6ac1
305 <305@305-PC>
2020-04-20 11:35:53 +0900

    first

---
+++ 20200420/src/Ex03.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("숫자 입력하세요");
+		int a = Integer.parseInt(scan.nextLine());
+		
+		System.out.println("숫자 입력하세요");
+		int b = scan.nextInt();
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		
+		if(a>b) {
+			System.out.println("a 가 큽니다.");
+		}
+		else if (a<b) {
+			System.out.println("b 가 큽니다.");
+		}
+		else {
+			System.out.println("a와 b가 같습니다.");
+		}
+	}
+}
